0d3b469a22d810bc1efd989ec528a04f5f4b73e4
Istvan Elekes <istvan.elekes@partner.rolls-roycemotorcars.com>
2024-09-04 14:21:10 +0300

    Token deployment

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -3,7 +3,7 @@ async function main() {
     const Token = await ethers.getContractFactory("Token")
     
     // deploy Contract
-    const token = await Token.deploy()
+    const token = await Token.deploy('Dapp University', 'DAPPU', '1000000')
     await token.deployed()
     console.log(`Token deployed to: ${token.address}`)
 }
