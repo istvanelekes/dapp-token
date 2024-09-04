5939f12e6b0a6493e9c804562b24794182d059bc
Istvan Elekes <istvan.elekes@partner.rolls-roycemotorcars.com>
2024-09-04 12:22:53 +0300

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -24,14 +24,19 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns(bool success) {
-        require(balanceOf[msg.sender] >= _value, 'Not enough token to spend');
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
 
-        balanceOf[msg.sender] -= _value;
+        balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
 
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _value, 'Not enough token to spend');
+        
+        _transfer(msg.sender, _to, _value);
 
         return true;
     }
@@ -44,5 +49,17 @@ contract Token {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] -= _value;
+
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+
 
 }
